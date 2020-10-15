1c2e89cca0a591c371d1a9d257a367772054e61d
Kate Skocelas <kgskocelas@gmail.com>
2020-10-15 11:42:32 -0400

    Kate's plot working for real this time

--- skocelas_project1.R
+++ skocelas_project1.R
@@ -15,12 +15,12 @@ library(dplyr)
 library(ggplot2)
 
 # Create bar plot of metamorphic stage
-ggplot(butterfly_data, aes(x=phyenotype..metamorphosis.stage.))+
-  geom_bar(col="Black", fill="Navy")+
-  labs(x="Metamorphic Stage", y="Number of Samples", title="Metamorphic Stage of Samples")+
-  theme_classic()
+#ggplot(butterfly_data, aes(x=phyenotype..metamorphosis.stage.))+
+#  geom_bar(col="Black", fill="Navy")+
+#  labs(x="Metamorphic Stage", y="Number of Samples", title="Metamorphic Stage of Samples")+
+#  theme_classic()
 
-#TODO plot stage against date of observation
+#plot stage against date of observation
 butterfly_data$month <- substr(butterfly_data$observed_on, 1,2)
 for(i in 1:nrow(butterfly_data)) {
   month <- butterfly_data$month[i]
@@ -31,44 +31,10 @@ for(i in 1:nrow(butterfly_data)) {
   butterfly_data$month[i] <- month
 }
 
-#read in date info in format 'm/d/y'
-#butterfly_data$date <- as.Date(butterfly_data$observed_on, "%m/%d/%y")
-
-# make a 12 value vector for each life stage where each entry is the # of observations of that life stage in that month
-egg <- integer(12)
-chrysalis <- integer(12)
-caterpillar <- integer(12)
-butterfly <- integer(12)
-
 phenotype <- butterfly_data$phyenotype..metamorphosis.stage.
-for(r in 1:nrow(butterfly_data)) {
-  phenotype <- butterfly_data[r, 15]
-  month <- as.numeric(butterfly_data$month[r])
-  if(phenotype == "egg") {
-    egg[month] <- egg[month] + 1
-  }
-  else if(phenotype == "chrysalis"){
-    chrysalis[month] <- chrysalis[month] + 1
-  }
-  else if(phenotype == "caterpillar"){
-    caterpillar[month] <- caterpillar[month] + 1
-  }
-  else if(phenotype == "butterfly"){
-    butterfly[month] <- butterfly[month] + 1
-  }
-  else {
-    print("Error: No phenotype match!")
-  }
-}
+butterfly_data$month <- factor(butterfly_data$month, levels=c("1","2","3","4","5","6","7","8","9","10","11","12"))
 
-# make a blank plot, then add the 4 lines (1 for each life stage)
-months <- c(1,2,3,4,5,6,7,8,9,10,11,12)
-df <- data.frame(months, egg, chrysalis, caterpillar, butterfly)
-
-ggplot(df, aes(months))+
-  geom_line(aes(y=egg, color="orange"))+
-  geom_line(aes(y=chrysalis, color="red"))+
-  geom_line(aes(y=caterpillar, color="blue"))+
-  geom_line(aes(y=butterfly, color="purple"))+
+ggplot(butterfly_data, aes(x=month, fill=phenotype))+
+  geom_bar()+
   labs(x="Month", y="Number of Samples", title="Metamorphic Stage of Samples by Month")+
   theme_classic()
